9ea8da22feff0110d9543abeb7e985c542c3658c
ChristianMN <christian.munknissen@gmail.com>
2023-04-25 09:20:28 +0200

    first commit

---
+++ src/main/java/com/example/projectpeak1/ProjectPeak1Application.java
@@ -0,0 +1,13 @@
+package com.example.projectpeak1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPeak1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPeak1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectpeak1/ProjectPeak1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpeak1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPeak1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
