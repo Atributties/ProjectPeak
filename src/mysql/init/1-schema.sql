cbafbce3d3c4f6447ccd94a41c47f375e49215b2
ChristianMN <christian.munknissen@gmail.com>
2023-05-03 13:00:01 +0200

    sql scirpt

--- src/mysql/init/1-schema.sql
+++ src/mysql/init/1-schema.sql
@@ -3,41 +3,45 @@ CREATE DATABASE projectPeak;
 USE projectPeak;
 
 CREATE TABLE User (
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT AUTO_INCREMENT,
   fullname VARCHAR(255),
   email VARCHAR(255),
   user_password VARCHAR(255),
-  role VARCHAR(255)
+  role VARCHAR(255),
+  PRIMARY KEY(user_id)
 );
 
 CREATE TABLE Project (
-  id INT PRIMARY KEY,
+  project_id INT AUTO_INCREMENT,
   name VARCHAR(255),
   description VARCHAR(255),
   start_date DATE,
   end_date DATE,
   user_id INT,
+  PRIMARY KEY(project_id),
   FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
 CREATE TABLE Task (
-  id INT PRIMARY KEY,
+  task_id INT AUTO_INCREMENT,
   name VARCHAR(255),
   description VARCHAR(255),
   start_date DATE,
   end_date DATE,
   status VARCHAR(255),
   project_id INT,
-  FOREIGN KEY (project_id) REFERENCES Project(id)
+  PRIMARY KEY(task_id),
+  FOREIGN KEY (project_id) REFERENCES Project(project_id)
 );
 
 CREATE TABLE Subtask (
-  id INT PRIMARY KEY,
+  subtask_id INT AUTO_INCREMENT,
   name VARCHAR(255),
   description VARCHAR(255),
   start_date DATE,
   end_date DATE,
   status VARCHAR(255),
   task_id INT,
-  FOREIGN KEY (task_id) REFERENCES Task(id)
+  PRIMARY KEY(subtask_id),
+  FOREIGN KEY (task_id) REFERENCES Task(task_id)
 );
