3cfc4dfe4fc8516a12216a9fc8af5847eace3584
marcusFN <marc599n@stud.kea.dk>
2023-05-07 13:05:45 +0200

    setter and constructors

--- src/main/java/com/example/projectpeak1/entities/SubTask.java
+++ src/main/java/com/example/projectpeak1/entities/SubTask.java
@@ -13,4 +13,61 @@ public class SubTask {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate subTaskEndDate;
     private int userId;
+
+    public SubTask(int subTaskId, String subTaskName, String subTaskDescription, LocalDate subTaskStartDate, LocalDate subTaskEndDate, int userId) {
+        this.subTaskId = subTaskId;
+        SubTaskName = subTaskName;
+        this.subTaskDescription = subTaskDescription;
+        this.subTaskStartDate = subTaskStartDate;
+        this.subTaskEndDate = subTaskEndDate;
+        this.userId = userId;
+    }
+
+    public int getSubTaskId() {
+        return subTaskId;
+    }
+
+    public void setSubTaskId(int subTaskId) {
+        this.subTaskId = subTaskId;
+    }
+
+    public String getSubTaskName() {
+        return SubTaskName;
+    }
+
+    public void setSubTaskName(String subTaskName) {
+        SubTaskName = subTaskName;
+    }
+
+    public String getSubTaskDescription() {
+        return subTaskDescription;
+    }
+
+    public void setSubTaskDescription(String subTaskDescription) {
+        this.subTaskDescription = subTaskDescription;
+    }
+
+    public LocalDate getSubTaskStartDate() {
+        return subTaskStartDate;
+    }
+
+    public void setSubTaskStartDate(LocalDate subTaskStartDate) {
+        this.subTaskStartDate = subTaskStartDate;
+    }
+
+    public LocalDate getSubTaskEndDate() {
+        return subTaskEndDate;
+    }
+
+    public void setSubTaskEndDate(LocalDate subTaskEndDate) {
+        this.subTaskEndDate = subTaskEndDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
--- src/main/java/com/example/projectpeak1/entities/Task.java
+++ src/main/java/com/example/projectpeak1/entities/Task.java
@@ -14,6 +14,60 @@ public class Task {
     private LocalDate taskEndDate;
     private int userId;
 
+    public Task(int taskId, String taskName, String taskDescription, LocalDate taskStartDate, LocalDate taskEndDate, int userId) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskStartDate = taskStartDate;
+        this.taskEndDate = taskEndDate;
+        this.userId = userId;
+    }
 
+    public int getTaskId() {
+        return taskId;
+    }
 
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getTaskStartDate() {
+        return taskStartDate;
+    }
+
+    public void setTaskStartDate(LocalDate taskStartDate) {
+        this.taskStartDate = taskStartDate;
+    }
+
+    public LocalDate getTaskEndDate() {
+        return taskEndDate;
+    }
+
+    public void setTaskEndDate(LocalDate taskEndDate) {
+        this.taskEndDate = taskEndDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
