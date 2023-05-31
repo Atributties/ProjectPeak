d4a8d989f26d4ab67f059fec9cf669e7e0b76b7d
Mathias-Thomsen <math831t6@stud.kea.dk>
2023-05-31 14:03:46 +0200

    Update sql scripts

--- src/mysql/init/1-schema.sql
+++ src/mysql/init/1-schema.sql
@@ -3,89 +3,88 @@ CREATE DATABASE projectPeak;
 USE projectPeak;
 
 CREATE TABLE User (
-  user_id INT AUTO_INCREMENT,
-  fullname VARCHAR(255),
-  email VARCHAR(255),
-  user_password VARCHAR(255),
-  PRIMARY KEY(user_id)
+user_id INT AUTO_INCREMENT,
+fullname VARCHAR(255),
+email VARCHAR(255),
+user_password VARCHAR(255),
+PRIMARY KEY(user_id)
 );
 
 CREATE TABLE Project (
-  project_id INT AUTO_INCREMENT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  user_id INT,
-  PRIMARY KEY(project_id),
-  FOREIGN KEY(user_id) REFERENCES User(user_id)
+project_id INT AUTO_INCREMENT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+user_id INT,
+PRIMARY KEY(project_id)
 );
 
 CREATE TABLE ProjectMember (
-  project_member_id INT AUTO_INCREMENT,
-  project_id INT,
-  user_id INT,
-  PRIMARY KEY(project_member_id),
-  FOREIGN KEY(project_id) REFERENCES Project(project_id) ON DELETE CASCADE,
-  FOREIGN KEY(user_id) REFERENCES User(user_id)
+project_member_id INT AUTO_INCREMENT,
+project_id INT,
+user_id INT,
+PRIMARY KEY(project_member_id),
+FOREIGN KEY(project_id) REFERENCES Project(project_id) ON DELETE CASCADE,
+FOREIGN KEY(user_id) REFERENCES User(user_id)
 );
 
 CREATE TABLE Task (
-  task_id INT AUTO_INCREMENT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  status VARCHAR(255),
-  project_id INT,
-  PRIMARY KEY(task_id),
-  FOREIGN KEY(project_id) REFERENCES Project(project_id)
+task_id INT AUTO_INCREMENT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+status VARCHAR(255),
+project_id INT,
+PRIMARY KEY(task_id),
+FOREIGN KEY(project_id) REFERENCES Project(project_id)
 );
 
 CREATE TABLE Subtask (
-  subtask_id INT AUTO_INCREMENT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  status VARCHAR(255),
-  task_id INT,
-  PRIMARY KEY(subtask_id),
-  FOREIGN KEY(task_id) REFERENCES Task(task_id)
+subtask_id INT AUTO_INCREMENT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+status VARCHAR(255),
+task_id INT,
+PRIMARY KEY(subtask_id),
+FOREIGN KEY(task_id) REFERENCES Task(task_id)
 );
 
 CREATE TABLE DoneProject (
-  project_id INT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  project_completed_date DATE DEFAULT (CURRENT_DATE),
-  user_id INT,
-  PRIMARY KEY(project_id)
+project_id INT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+project_completed_date DATE DEFAULT (CURRENT_DATE),
+user_id INT,
+PRIMARY KEY(project_id)
 );
 
 
 CREATE TABLE DoneTask (
-  task_id INT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  task_completed_date DATE DEFAULT (CURRENT_DATE),
-  status VARCHAR(255),
-  project_id INT,
-  PRIMARY KEY(task_id)
+task_id INT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+task_completed_date DATE DEFAULT (CURRENT_DATE),
+status VARCHAR(255),
+project_id INT,
+PRIMARY KEY(task_id)
 );
 
 CREATE TABLE DoneSubtask (
-  subtask_id INT,
-  name VARCHAR(255),
-  description VARCHAR(255),
-  start_date DATE,
-  end_date DATE,
-  subtask_completed_date DATE DEFAULT (CURRENT_DATE),
-  status VARCHAR(255),
-  task_id INT,
-  PRIMARY KEY(subtask_id)
+subtask_id INT,
+name VARCHAR(255),
+description VARCHAR(255),
+start_date DATE,
+end_date DATE,
+subtask_completed_date DATE DEFAULT (CURRENT_DATE),
+status VARCHAR(255),
+task_id INT,
+PRIMARY KEY(subtask_id)
 );
--- src/mysql/init/2-data.sql
+++ src/mysql/init/2-data.sql
@@ -7,8 +7,8 @@ INSERT INTO User (fullname, email, user_password) VALUES
 
 -- Projects
 INSERT INTO Project (name, description, start_date, end_date, user_id) VALUES
-('Project 1', 'This is project 1', '2023-05-22', '2023-06-22', 1),
-('Project 2', 'This is project 2', '2023-05-29', '2023-07-23', 2);
+('Project 1', 'This is project 1', '2023-07-22', '2023-08-22', 1),
+('Project 2', 'This is project 2', '2023-08-29', '2023-09-23', 2);
 
 -- ProjectMember
 INSERT INTO ProjectMember (project_id, user_id) VALUES
@@ -17,11 +17,56 @@ INSERT INTO ProjectMember (project_id, user_id) VALUES
 
 -- Tasks
 INSERT INTO Task (name, description, start_date, end_date, status, project_id) VALUES
-('Task', 'This is task 1.1 for project 1', '2023-05-22', '2023-05-26', 'Completed', 1),
-('Task', 'This is task 1.2 for project 1', '2023-05-23', '2023-05-24', 'Completed', 1),
-('Task', 'This is task 2.1 for project 2', '2023-05-29', '2023-06-15', 'In progress', 2);
+('Task 1.1', 'This is task 1.1 for project 1', '2023-07-22', '2023-07-24', 'Not Started', 1),
+('Task 1.2', 'This is task 1.2 for project 1', '2023-07-22', '2023-07-26', 'Not Started', 1),
+('Task 1.3', 'This is task 1.3 for project 1', '2023-07-26', '2023-07-28', 'Not Started', 1),
+('Task 1.4', 'This is task 1.4 for project 1', '2023-07-27', '2023-08-03', 'Not Started', 1),
+('Task 1.5', 'This is task 1.5 for project 1', '2023-07-31', '2023-08-06', 'Not Started', 1),
+('Task 1.6', 'This is task 1.6 for project 1', '2023-08-10', '2023-08-22', 'Not Started', 1),
+('Task 1.1', 'This is task 1.1 for project 2', '2023-07-22', '2023-07-24', 'Not Started', 2),
+('Task 1.2', 'This is task 1.2 for project 2', '2023-07-22', '2023-07-26', 'Not Started', 2),
+('Task 1.3', 'This is task 1.3 for project 2', '2023-07-26', '2023-07-28', 'Not Started', 2),
+('Task 1.4', 'This is task 1.4 for project 2', '2023-07-27', '2023-08-03', 'Not Started', 2),
+('Task 1.5', 'This is task 1.5 for project 2', '2023-07-31', '2023-08-06', 'Not Started', 2),
+('Task 1.6', 'This is task 1.6 for project 2', '2023-08-10', '2023-08-22', 'Not Started', 2);
+
+
 -- Subtasks
 INSERT INTO Subtask (name, description, start_date, end_date, status, task_id) VALUES
-('Subtask 1.1.1', 'This is subtask 1 for task 1.1', '2023-05-22', '2023-05-23', 'Completed', 1),
-('Subtask 1.1.2', 'This is subtask 2 for task 1.1', '2023-05-23', '2023-05-26', 'Completed', 1),
-('Subtask 2.1.1', 'This is subtask 1 for task 2.1', '2023-02-01', '2023-02-15', 'In progress', 3);
+('Subtask 1.1.1', 'This is subtask 1 for task 1.1', '2023-07-22', '2023-07-23', 'Not Started', 1),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.1', '2023-07-23', '2023-07-24', 'Not Started', 1),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.1', '2023-07-22', '2023-07-24', 'Not Started', 1),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.2', '2023-07-22', '2023-07-25', 'Not Started', 2),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.2', '2023-07-23', '2023-07-26', 'Not Started', 2),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.2', '2023-07-25', '2023-07-26', 'Not Started', 2),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.3', '2023-07-26', '2023-07-27', 'Not Started', 3),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.3', '2023-07-26', '2023-07-28', 'Not Started', 3),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.3', '2023-07-27', '2023-07-28', 'Not Started', 3),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.4', '2023-07-27', '2023-07-28', 'Not Started', 4),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.4', '2023-07-29', '2023-08-01', 'Not Started', 4),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.4', '2023-08-01', '2023-08-03', 'Not Started', 4),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.5', '2023-07-31', '2023-08-01', 'Not Started', 5),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.5', '2023-08-01', '2023-08-03', 'Not Started', 5),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.5', '2023-08-03', '2023-08-06', 'Not Started', 5),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.6', '2023-08-10', '2023-08-15', 'Not Started', 6),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.6', '2023-08-15', '2023-08-17', 'Not Started', 6),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.6', '2023-08-17', '2023-08-22', 'Not Started', 6),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.1', '2023-07-22', '2023-07-23', 'Not Started', 7),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.1', '2023-07-23', '2023-07-24', 'Not Started', 7),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.1', '2023-07-22', '2023-07-24', 'Not Started', 7),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.2', '2023-07-22', '2023-07-25', 'Not Started', 8),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.2', '2023-07-23', '2023-07-26', 'Not Started', 8),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.2', '2023-07-25', '2023-07-26', 'Not Started', 8),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.3', '2023-07-26', '2023-07-27', 'Not Started', 9),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.3', '2023-07-26', '2023-07-28', 'Not Started', 9),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.3', '2023-07-27', '2023-07-28', 'Not Started', 9),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.4', '2023-07-27', '2023-07-28', 'Not Started', 10),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.4', '2023-07-29', '2023-08-01', 'Not Started', 10),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.4', '2023-08-01', '2023-08-03', 'Not Started', 10),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.5', '2023-07-31', '2023-08-01', 'Not Started', 11),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.5', '2023-08-01', '2023-08-03', 'Not Started', 11),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.5', '2023-08-03', '2023-08-06', 'Not Started', 11),
+('Subtask 1.1.1', 'This is subtask 1 for task 1.6', '2023-08-10', '2023-08-15', 'Not Started', 12),
+('Subtask 1.1.2', 'This is subtask 2 for task 1.6', '2023-08-15', '2023-08-17', 'Not Started', 12),
+('Subtask 1.1.3', 'This is subtask 3 for task 1.6', '2023-08-17', '2023-08-22', 'Not Started', 12);
+
